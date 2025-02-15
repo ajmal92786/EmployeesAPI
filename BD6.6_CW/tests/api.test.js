dbb97d5fedd7ee8af46d8b68bd869536c0a8d180
ajmal92786 <mohdajmalraza92786@gmail.com>
2025-02-16 01:13:47 +0530

    Initial commit

---
+++ BD6.6_CW/tests/api.test.js
@@ -0,0 +1,104 @@
+const request = require('supertest');
+const http = require('http');
+const { getAllEmployees } = require('../controllers');
+const { app } = require('../index.js');
+
+jest.mock('../controllers', () => ({
+  ...jest.requireActual('../controllers'),
+  getAllEmployees: jest.fn(),
+}));
+
+let server;
+beforeAll(async () => {
+  server = http.createServer(app);
+  server.listen(3001);
+});
+
+afterAll(async () => {
+  server.close();
+});
+
+describe('Controller Function Test', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  // Exercise 5: Mock the Get All Employees Function
+  it('should return all employees', () => {
+    let mockedEmployees = [
+      {
+        employeeId: 1,
+        name: 'Rahul Sharma',
+        email: 'rahul.sharma@example.com',
+        departmentId: 1,
+        roleId: 1,
+      },
+      {
+        employeeId: 2,
+        name: 'Priya Singh',
+        email: 'priya.singh@example.com',
+        departmentId: 2,
+        roleId: 2,
+      },
+      {
+        employeeId: 3,
+        name: 'Ankit Verma',
+        email: 'ankit.verma@example.com',
+        departmentId: 1,
+        roleId: 3,
+      },
+    ];
+
+    getAllEmployees.mockReturnValue(mockedEmployees);
+    let result = getAllEmployees();
+    expect(result).toEqual(mockedEmployees);
+    expect(result.length).toBe(3);
+  });
+});
+
+describe('API Endpoint Tests', () => {
+  // Exercise 3: Test Retrieve All Employees
+  it('GET /employees should get all employees', async () => {
+    const res = await request(server).get('/employees');
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      employees: [
+        {
+          employeeId: 1,
+          name: 'Rahul Sharma',
+          email: 'rahul.sharma@example.com',
+          departmentId: 1,
+          roleId: 1,
+        },
+        {
+          employeeId: 2,
+          name: 'Priya Singh',
+          email: 'priya.singh@example.com',
+          departmentId: 2,
+          roleId: 2,
+        },
+        {
+          employeeId: 3,
+          name: 'Ankit Verma',
+          email: 'ankit.verma@example.com',
+          departmentId: 1,
+          roleId: 3,
+        },
+      ],
+    });
+    expect(res.body.employees.length).toBe(3);
+  });
+  // Exercise 4: Test Retrieve Employee by ID
+  it('GET /employees/details/:id should get an employee by ID', async () => {
+    const res = await request(server).get('/employees/details/1');
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      employee: {
+        employeeId: 1,
+        name: 'Rahul Sharma',
+        email: 'rahul.sharma@example.com',
+        departmentId: 1,
+        roleId: 1,
+      },
+    });
+  });
+});
